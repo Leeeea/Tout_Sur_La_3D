2a223d3a47ef2981cb7f4d73d39e679b7a57bdbc
Leeeea <56026014+Leeeea@users.noreply.github.com>
2020-02-10 13:04:53 +0100

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+   // MENU MENU MENU
+   function menu(){
+    document.querySelector("nav").
+    classList.toggle("translate")
+}
+
+function menuf(){
+    document.querySelector("nav").
+    classList.toggle("translate")
+}
